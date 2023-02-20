59bcc746a61a214d45c18b188c31f8d3cbd11099
gavtography <104710808+gavtography@users.noreply.github.com>
2023-02-20 01:54:10 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+const quizData = [
+  {
+    question: "What is your favorite color?",
+    answers: [
+      { text: "Red", value: "red" },
+      { text: "Blue", value: "blue" },
+      { text: "Green", value: "green" },
+      { text: "Yellow", value: "yellow" },
+    ],
+  },
+  {
+    question: "What is your favorite animal?",
+    answers: [
+      { text: "Dog", value: "dog" },
+      { text: "Cat", value: "cat" },
+      { text: "Bird", value: "bird" },
+      { text: "Fish", value: "fish" },
+    ],
+  },
+  // Add more questions and answers as needed
+];
