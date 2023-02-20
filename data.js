b69263ce05164b53fb914726750917f0e3353874
gavtography <104710808+gavtography@users.noreply.github.com>
2023-02-20 04:44:58 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,16 +1,26 @@
-const questions = [
-  { text: "Do you like the outdoors?", answer: null },
-  { text: "Do you like to play?", answer: null },
-  { text: "Do you like to sit around all day?", answer: null },
+const quizData = [
+  {
+    question: "Do you have Verizon service available?",
+    answers: ["Yes", "No"]
+  },
+  {
+    question: "Do you have T-Mobile service available?",
+    answers: ["Yes", "No"]
+  },
+  {
+    question: "Do you have AT&T service available?",
+    answers: ["Yes", "No"]
+  },
+  {
+    question: "Does this carrier provide a strong connection at your location?",
+    answers: ["Yes", "No"]
+  },
+  {
+    question: "Do you have Starlink available?",
+    answers: ["Yes", "No"]
+  },
+  {
+    question: "Are you comfortable trying unofficial (but legal) workarounds to getting access to rural internet?",
+    answers: ["Yes", "No"]
+  }
 ];
-
-const results = {
-  "000": "Your favorite pet is probably cats",
-  "001": "Your favorite pet is probably birds",
-  "010": "Your favorite pet is probably fish",
-  "011": "Your favorite pet is probably hamsters",
-  "100": "Your favorite pet is probably dogs",
-  "101": "Your favorite pet is probably snakes",
-  "110": "Your favorite pet is probably rabbits",
-  "111": "Your favorite pet is probably turtles",
-};
