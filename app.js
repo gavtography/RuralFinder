fc9abd2c49efc1ee905c03a5bf0058a1dbefb8e0
gavtography <104710808+gavtography@users.noreply.github.com>
2023-02-20 01:56:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
-const quizForm = document.getElementById('quiz-form');
-const nextButton = document.getElementById('next-button');
+const quizForm = document.getElementById("quiz-form");
+const nextButton = document.getElementById("next-button");
+const quizQuestionsContainer = document.getElementById("quiz-questions");
 
-quizForm.addEventListener('submit', (event) => {
+let currentQuestionIndex = 0;
+
+function showQuizQuestion() {
+  const currentQuestion = quizData[currentQuestionIndex];
+  const questionText = currentQuestion.question;
+
+  const answersHtml = currentQuestion.answers
+    .map(
+      (answer) =>
+        `<label>
+           <input type="radio" name="answer" value="${answer.value}">
+           ${answer.text}
+         </label>`
+    )
+    .join("");
+
+  quizQuestionsContainer.innerHTML = `
+    <div class="quiz-question">
+      <h2>${questionText}</h2>
+      ${answersHtml}
+    </div>
+  `;
+}
+
+function showNextQuestion() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < quizData.length) {
+    showQuizQuestion();
+    nextButton.setAttribute("disabled", true);
+  } else {
+    // Show quiz results
+    window.location.href = "results.html";
+  }
+}
+
+quizForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const selectedAnswer = document.querySelector('input[name="answer"]:checked');
 
   if (selectedAnswer) {
-    nextButton.removeAttribute('disabled');
-    window.location.href = 'success.html';
+    nextButton.removeAttribute("disabled");
+    showNextQuestion();
   } else {
-    alert('Please select an answer before continuing');
+    alert("Please select an answer before continuing");
   }
 });
+
+showQuizQuestion();
