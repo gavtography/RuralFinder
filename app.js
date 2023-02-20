c0c7394c1807f714f8b0060d36ab2fbdf09cb4e3
gavtography <104710808+gavtography@users.noreply.github.com>
2023-02-20 18:13:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,96 +1,140 @@
+// Define the variables
+const questionContainer = document.getElementById('question-container');
+const questionText = document.getElementById('question-text');
+const buttonContainer = document.getElementById('button-container');
+const yesButton = document.getElementById('yes-button');
+const noButton = document.getElementById('no-button');
+const resultContainer = document.getElementById('result-container');
+const resultText = document.getElementById('result-text');
+const additionalText = document.getElementById('additional-text');
+
+// Define the initial state
+let currentQuestion = 0;
+let userAnswers = [];
+
+// Define the questions and answers
 const questions = [
   {
-    text: 'Do you have Verizon service available?',
-    followUp: true
+    question: 'Do you have Verizon service available?',
+    followUp: true,
   },
   {
-    text: 'Do you have T-Mobile service available?',
-    followUp: true
+    question: 'Does this carrier provide a strong connection at your location?',
+    followUp: false,
   },
   {
-    text: 'Do you have AT&T service available?',
-    followUp: true
+    question: 'Do you have T-Mobile service available?',
+    followUp: true,
   },
   {
-    text: 'Do you have Starlink available?',
-    followUp: false
+    question: 'Does this carrier provide a strong connection at your location?',
+    followUp: false,
   },
   {
-    text: 'Are you comfortable trying unofficial (but legal) workarounds to getting access to rural internet?',
-    followUp: false
-  }
+    question: 'Do you have AT&T service available?',
+    followUp: true,
+  },
+  {
+    question: 'Does this carrier provide a strong connection at your location?',
+    followUp: false,
+  },
+  {
+    question: 'Do you have Starlink available?',
+    followUp: false,
+  },
+  {
+    question: 'Are you comfortable trying unofficial (but legal) workarounds to getting access to rural internet?',
+    followUp: false,
+  },
 ];
 
-let currentQuestionIndex = 0;
-let answers = {};
-
-const getNextQuestion = () => {
-  return questions[currentQuestionIndex];
+// Define the providers and their corresponding results
+const providers = {
+  'Verizon-Comfortable': 'Verizon 4G Home Internet or a Visible sim card in an LTE router',
+  'Verizon-Uncomfortable': 'Verizon 4G or Verizon 5G Home Internet',
+  'T-Mobile-Comfortable': 'T-Mobile Home Internet or Calyx Institute Internet Membership',
+  'T-Mobile-Uncomfortable': 'T-Mobile Home Internet or Calyx Institute Internet Membership',
+  'AT&T-Comfortable': 'AT&T Business Wireless Essentials',
+  'AT&T-Uncomfortable': 'non-unlimited AT&T fixed wireless',
+  'Starlink-Comfortable': 'Starlink',
+  'Starlink-Uncomfortable': 'Starlink RV',
 };
 
-const handleButtonClick = (answer) => {
-  answers[currentQuestionIndex] = answer;
-  if (currentQuestionIndex < questions.length - 1) {
-    currentQuestionIndex++;
-    showCurrentQuestion();
+// Define the function to display the current question
+function displayQuestion() {
+  questionText.innerHTML = questions[currentQuestion].question;
+  buttonContainer.style.display = 'flex';
+}
+
+// Define the function to handle the user's answer
+function handleAnswer(answer) {
+  userAnswers.push(answer);
+  if (currentQuestion < questions.length - 1) {
+    currentQuestion++;
+    if (questions[currentQuestion].followUp) {
+      displayQuestion();
+    } else {
+      buttonContainer.style.display = 'none';
+      displayResult();
+    }
   } else {
-    showResult();
+    buttonContainer.style.display = 'none';
+    displayResult();
   }
-};
+}
 
-const showCurrentQuestion = () => {
-  const questionContainer = document.getElementById('question-container');
-  const questionText = document.getElementById('question-text');
-  const buttonContainer = document.getElementById('button-container');
+// Define the function to display the result
+function displayResult() {
+  let providerKey = '';
+  let comfortLevel = '';
+  let result = '';
 
-  questionText.textContent = getNextQuestion().text;
-
-  // Remove any existing buttons
-  while (buttonContainer.firstChild) {
-    buttonContainer.removeChild(buttonContainer.firstChild);
-  }
-
-  // Add yes and no buttons
-  const yesButton = document.createElement('button');
-  yesButton.textContent = 'Yes';
-  yesButton.id = 'yes-button';
-  yesButton.addEventListener('click', () => handleButtonClick('yes'));
-  buttonContainer.appendChild(yesButton);
-
-  const noButton = document.createElement('button');
-  noButton.textContent = 'No';
-  noButton.id = 'no-button';
-  noButton.addEventListener('click', () => handleButtonClick('no'));
-  buttonContainer.appendChild(noButton);
-
-  // If this question has a follow-up question, add a skip button
-  const question = getNextQuestion();
-  if (question.followUp) {
-    const skipButton = document.createElement('button');
-    skipButton.textContent = 'Skip';
-    skipButton.id = 'skip-button';
-    skipButton.addEventListener('click', () => handleButtonClick('skip'));
-    buttonContainer.appendChild(skipButton);
-  }
-};
-
-const showResult = () => {
-  const resultContainer = document.getElementById('result-container');
-  const resultText = document.getElementById('result-text');
-  const additionalText = document.getElementById('additional-text');
-
-  const result = getProvider(answers);
-
-  resultText.textContent = `Based on your answers, the best rural internet provider for you is ${result.name}.`;
-  if (result.additionalText) {
-    additionalText.textContent = result.additionalText;
+  // Determine the provider and comfort level based on the user's answers
+  if (userAnswers[0] === 'Yes') {
+    if (userAnswers[1] === 'Yes') {
+      providerKey = 'Verizon-Comfortable';
+      comfortLevel = 'comfortable';
+    } else if (userAnswers[1] === 'No') {
+      providerKey = 'Verizon-Uncomfortable';
+      comfortLevel = 'uncomfortable';
+    }
+  } else if (userAnswers[2] === 'Yes') {
+    if (userAnswers[3] === 'Yes') {
+      providerKey = 'T-Mobile-Comfortable';
+      comfortLevel = 'comfortable';
+    } else if (userAnswers[3] === 'No') {
+      providerKey = 'T-Mobile-Uncomfortable';
+      comfortLevel = 'uncomfortable';
+    }
+  } else if (userAnswers[4] === 'Yes') {
+    if (userAnswers[5] === 'Yes') {
+      providerKey = 'AT&T-Comfortable';
+      comfortLevel = 'comfortable';
+    } else if (userAnswers[5] === 'No') {
+      providerKey = 'AT&T-Uncomfortable';
+      comfortLevel = 'uncomfortable';
+    }
+  } else if (userAnswers[6] === 'Yes') {
+    if (userAnswers[7] === 'Yes') {
+      providerKey = 'Starlink-Comfortable';
+      comfortLevel = 'comfortable';
+    } else if (userAnswers[7] === 'No') {
+      providerKey = 'Starlink-Uncomfortable';
+      comfortLevel = 'uncomfortable';
+    }
   }
 
+  // Determine the result text based on the provider and comfort level
+  if (providerKey !== '') {
+    result = providers[providerKey];
+    if (comfortLevel === 'uncomfortable') {
+      additionalText.innerHTML = 'Note: It may be difficult to get a strong connection with this provider at your location.';
+    }
+  } else {
+    result = "We're sorry, we could not find a suitable internet provider for you. Please contact your local government representative to advocate for improved internet access in your area.";
+  }
+
+  // Update the result container with the appropriate text
   resultContainer.style.display = 'block';
-};
-
-const start = () => {
-  showCurrentQuestion();
-};
-
-start();
+  resultText.innerHTML = result;
+}
