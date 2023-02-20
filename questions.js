b76167bcb5d0fba8c13561d56220af5b01f099db
gavtography <104710808+gavtography@users.noreply.github.com>
2023-02-20 18:06:09 -0500

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,32 @@
+const questions = [
+  {
+    id: 1,
+    text: "Do you have Verizon service available?",
+    hasFollowUp: true,
+    followUpText: "Does Verizon provide a strong connection at your location?"
+  },
+  {
+    id: 2,
+    text: "Do you have T-Mobile service available?",
+    hasFollowUp: true,
+    followUpText: "Does T-Mobile provide a strong connection at your location?"
+  },
+  {
+    id: 3,
+    text: "Do you have AT&T service available?",
+    hasFollowUp: true,
+    followUpText: "Does AT&T provide a strong connection at your location?"
+  },
+  {
+    id: 4,
+    text: "Do you have Starlink available?",
+    hasFollowUp: false,
+    followUpText: ""
+  },
+  {
+    id: 5,
+    text: "Are you comfortable trying unofficial (but legal) workarounds to getting access to rural internet?",
+    hasFollowUp: false,
+    followUpText: ""
+  }
+];
