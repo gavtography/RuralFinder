26be69dd1c221dbd5535ca50ac7f52a6a95ab696
gavtography <104710808+gavtography@users.noreply.github.com>
2023-02-23 21:04:00 -0500

    Update survey.js

--- survey.js
+++ survey.js
@@ -1,43 +1,75 @@
 const questions = [
-	"Question 1: Do you like pizza?",
-	"Question 2: Have you ever been skydiving?",
-	"Question 3: Do you prefer coffee or tea?",
-	"Question 4: Are you afraid of spiders?",
-	"Question 5: Have you ever traveled to another country?",
-	"Question 6: Do you enjoy hiking?"
+  {
+    text: "Do you have Verizon service available?",
+    yes: "Do you feel comfortable with workarounds?",
+    no: "Do you have T-Mobile service available?"
+  },
+  {
+    text: "Do you have T-Mobile service available?",
+    yes: "Do you feel comfortable with workarounds?",
+    no: "Do you have AT&T service available?"
+  },
+  {
+    text: "Do you have AT&T service available?",
+    yes: "Do you feel comfortable with workarounds?",
+    no: "Does this carrier provide a strong connection at your location?"
+  },
+  {
+    text: "Do any one of those carriers provide a strong connection at your location?",
+    yes: "Do you have Starlink available?",
+    no: "LTE router with external antenna or external antenna modification to hardware may be required."
+  },
+  {
+    text: "Do you have Starlink available?",
+    yes: "Do you feel comfortable with workarounds?",
+    no: "Are you comfortable trying unofficial (but legal) workarounds to getting access to rural internet?"
+  },
+  {
+    text: "Are you comfortable trying unofficial (but legal) workarounds to getting access to rural internet?",
+    yes: "Starlink RV. Also suggest to visit the FCC Broadband Map to check their options.",
+    no: "Starlink RV. Also suggest to visit the FCC Broadband Map to check their options."
+  }
 ];
 
-const answerButtons = document.querySelectorAll("button");
-const questionText = document.getElementById("question-text");
-const resultContainer = document.getElementById("result-container");
-const additionalText = document.getElementById("additional-text");
-
-let currentQuestion = 0;
-let answers = [];
-
-function showQuestion() {
-	questionText.innerText = questions[currentQuestion];
-}
+const results = {
+  "verizon-no-workarounds": "Verizon 4G or Verizon 5G Home Internet",
+  "verizon-workarounds": "Verizon 4G Home Internet or a Visible sim card in an LTE router",
+  "t-mobile-no-workarounds": "T-Mobile Home Internet or Calyx Institute Internet Membership",
+  "t-mobile-workarounds": "T-Mobile Home Internet or Calyx Institute Internet Membership",
+  "at&t-no-workarounds": "Non-unlimited AT&T fixed wireless",
+  "at&t-workarounds": "AT&T Business Wireless Essentials",
+  "starlink-no-workarounds": "Starlink",
+  "starlink-workarounds": "Starlink"
+};
 
 function showResult() {
-	resultContainer.style.display = "block";
-	questionContainer.style.display = "none";
+  const answerContainer = document.getElementById("answer-container");
+  answerContainer.style.display = "none";
+  const resultContainer = document.getElementById("result-container");
+  resultContainer.style.display = "block";
+  const resultText = document.getElementById("result-text");
+  let result = "";
 
-	let answerCount = answers.filter(answer => answer === "Yes").length;
-	additionalText.innerText = `You answered yes to ${answerCount} questions out of 6.`;
-}
+  const verizonAvailable = answers[0] === "yes";
+  const tmobileAvailable = answers[1] === "yes";
+  const attAvailable = answers[2] === "yes";
+  const carrierHasStrongConnection = answers[3] === "yes";
+  const starlinkAvailable = answers[4] === "yes";
+  const comfortableWithWorkarounds = answers[5] === "yes";
 
-function answerQuestion(event) {
-	answers[currentQuestion] = event.target.id === "yes-button" ? "Yes" : "No";
-	currentQuestion++;
-
-	if (currentQuestion === questions.length) {
-		showResult();
-	} else {
-		showQuestion();
-	}
-}
-
-answerButtons.forEach(button => button.addEventListener("click", answerQuestion));
-
-showQuestion();
+  if (verizonAvailable) {
+    if (comfortableWithWorkarounds) {
+      result = results["verizon-workarounds"];
+    } else {
+      result = results["verizon-no-workarounds"];
+    }
+  } else if (tmobileAvailable) {
+    if (comfortableWithWorkarounds) {
+      result = results["t-mobile-workarounds"];
+    } else {
+      result = results["t-mobile-no-workarounds"];
+    }
+  } else if (attAvailable) {
+    if (comfortableWithWorkarounds) {
+      result = results["at&t-workarounds"];
+    } else
