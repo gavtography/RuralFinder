cbb56604d7a0c9566618744c242fd0979b387593
gavtography <104710808+gavtography@users.noreply.github.com>
2023-02-23 21:33:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,173 @@
-// Array of questions
-const questions = [  "Do you have Verizon service available?",  "Do you have T-Mobile service available?",  "Do you have AT&T service available?",  "Do any one of those carriers provide a strong connection at your location?",  "Do you have Starlink available?",  "Are you comfortable trying unofficial (but legal) workarounds to getting access to rural internet?"];
-
-// Array of possible results
-const results = [  "Verizon 4G or Verizon 5G Home Internet",  "Verizon 4G Home Internet or a Visible sim card in an LTE router",  "T-Mobile Home Internet or Calyx Institute Internet Membership",  "T-Mobile Home Internet or Calyx Institute Internet Membership",  "non-unlimited AT&T fixed wireless",  "AT&T Business Wireless Essentials",  "LTE router with external antenna or external antenna modification to hardware may be required",  "Starlink",  "Starlink",  "Starlink RV. Please check the FCC Broadband Map for other options.",  "Starlink RV. Please check the FCC Broadband Map for other options."];
-
-// Array of requirements for each result
-const requirements = [  ["Verizon"], // Requires Verizon service
-  ["Verizon"], // Requires Verizon service
-  ["T-Mobile"], // Requires T-Mobile service
-  ["T-Mobile"], // Requires T-Mobile service
-  ["AT&T"], // Requires AT&T service
-  ["AT&T"], // Requires AT&T service
-  [], // No specific requirements
-  ["Starlink"], // Requires Starlink service
-  ["Starlink"], // Requires Starlink service
-  [], // No specific requirements
-  [] // No specific requirements
+// Define survey questions as an array of objects
+const surveyQuestions = [
+  {
+    question: 'Do you have Verizon service available?',
+    yesResult: ['Verizon 4G or 5G Home Internet'],
+    noResult: []
+  },
+  {
+    question: 'Do you have T-Mobile service available?',
+    yesResult: ['T-Mobile Home Internet', 'Calyx Institute Internet Membership'],
+    noResult: []
+  },
+  {
+    question: 'Do you have AT&T service available?',
+    yesResult: ['Non-unlimited AT&T fixed wireless', 'AT&T Business Wireless Essentials'],
+    noResult: []
+  },
+  {
+    question: 'Do any one of those carriers provide a strong connection at your location?',
+    yesResult: [],
+    noResult: ['LTE router with external antenna or external antenna modification to hardware may be required']
+  },
+  {
+    question: 'Do you have Starlink available?',
+    yesResult: ['Starlink'],
+    noResult: []
+  },
+  {
+    question: 'Are you comfortable trying unofficial (but legal) workarounds to getting access to rural internet?',
+    yesResult: ['Verizon 4G Home Internet', 'Visible sim card in an LTE router', 'T-Mobile Home Internet', 'Calyx Institute Internet Membership', 'AT&T Business Wireless Essentials', 'Starlink'],
+    noResult: []
+  }
 ];
 
-// Current question number
-let currentQuestion = 0;
+// Set up survey variables
+let questionIndex = 0;
+let result = [];
 
-// User's answers
-let answers = [];
+// Get HTML elements
+const questionElement = document.getElementById('question');
+const yesButton = document.getElementById('yesBtn');
+const noButton = document.getElementById('noBtn');
 
-// Get necessary elements
-const questionText = document.getElementById("question");
-const yesButton = document.getElementById("yesButton");
-const noButton = document.getElementById("noButton");
-const resultText = document.getElementById("result");
-
-// Function to display next question
+// Function to display the next question
 function displayNextQuestion() {
-  // Display current question
-  questionText.innerText = questions[currentQuestion];
+  // Get the next question
+  const question = surveyQuestions[questionIndex].question;
+  // Display the question text
+  questionElement.textContent = question;
 }
 
-// Function to handle answer selection
-function handleAnswerSelection(answer) {
-  // Add answer to answers array
-  answers.push(answer);
-  // If last question, display result
-  if (currentQuestion === questions.length - 1) {
-    // Determine result based on answers
-    let result = "Result not found";
-    for (let i = 0; i < results.length; i++) {
-      if (requirements[i].every(r => answers.includes(r))) {
-        result = results[i];
-        break;
-      }
-    }
-    // Add extra text if required
-    if (!answers[3]) {
-      result += " LTE router with external antenna or external antenna modification to hardware may be required.";
-    }
-    // Display result
-    resultText.innerText = result;
-    // Hide buttons
-    yesButton.style.display = "none";
-    noButton.style.display = "none";
+// Function to add a result to the result array and move to the next question
+function addResultAndMoveToNextQuestion(resultToAdd) {
+  // Add the result to the result array
+  result = result.concat(resultToAdd);
+  // Move to the next question
+  questionIndex++;
+  // If we have displayed all the questions, display the final result
+  if (questionIndex >= surveyQuestions.length) {
+    displayFinalResult();
   } else {
-    // Increment current question
-    currentQuestion++;
-    // Display next question
+    // Display the next question
     displayNextQuestion();
   }
 }
 
-// Add event listeners to buttons
-yesButton.addEventListener("click", () => handleAnswerSelection(requirements[currentQuestion][0]));
-noButton.addEventListener("click", () => handleAnswerSelection(""));
+// Function to display the final result
+function displayFinalResult() {
+  let finalResult;
+  if (result.includes('Starlink')) {
+    if (result.includes('Verizon 4G Home Internet') || result.includes('Visible sim card in an LTE router') || result.includes('T-Mobile Home Internet') || result.includes('Calyx Institute Internet Membership') || result.includes('AT&T Business Wireless Essentials')) {
+      finalResult = 'Starlink';
+    } else {
+      finalResult = 'Starlink RV. Also suggest to visit the FCC Broadband Map to check your options.';
+    }
+  } else {
+    finalResult = result.join(', ');
+    if (result.length === 0) {
+      finalResult = 'No options found. Suggest to visit the FCC Broadband Map to check your options.';
+    }
+  }
+  // Display the final result text
+  questionElement.textContent = finalResult;
+  // Hide the Yes and No buttons
+  yesButton.style.display = 'none';
+  noButton.style.display = 'none';
+}
 
-// Display first question
-displayNextQuestion();
+// Event listener for Yes button
+yesBtn.addEventListener("click", function() {
+  // Check if a response has been selected
+  if (!responseSelected) {
+    alert("Please select a response before continuing");
+    return;
+  }
+  
+  // Check if this is the last question
+  if (currentQuestionIndex === questions.length - 1) {
+    showResult();
+  } else {
+    // Increment current question index and display the next question
+    currentQuestionIndex++;
+    displayQuestion();
+  }
+});
+
+// Event listener for No button
+noBtn.addEventListener("click", function() {
+  // Check if a response has been selected
+  if (!responseSelected) {
+    alert("Please select a response before continuing");
+    return;
+  }
+  
+  // Check if this is the last question
+  if (currentQuestionIndex === questions.length - 1) {
+    showResult();
+  } else {
+    // Increment current question index and display the next question
+    currentQuestionIndex++;
+    displayQuestion();
+  }
+});
+
+// Function to display the result based on the user's responses
+function showResult() {
+  let result;
+  let response = responses[responses.length - 1];
+  let verizon = responses.includes("verizon");
+  let tMobile = responses.includes("tMobile");
+  let att = responses.includes("att");
+  let starlink = responses.includes("starlink");
+  let workarounds = responses.includes("workarounds");
+  let strongConnection = responses.includes("strongConnection");
+  
+  if (verizon) {
+    if (workarounds) {
+      result = "Verizon 4G Home Internet or Visible sim card in an LTE router";
+    } else {
+      result = "Verizon 4G or Verizon 5G Home Internet";
+    }
+  } else if (tMobile) {
+    if (workarounds) {
+      result = "T-Mobile Home Internet or Calyx Institute Internet Membership";
+    } else {
+      result = "T-Mobile Home Internet or Calyx Institute Internet Membership";
+    }
+  } else if (att) {
+    if (workarounds) {
+      result = "AT&T Business Wireless Essentials";
+    } else {
+      result = "non-unlimited AT&T fixed wireless";
+    }
+  } else if (starlink) {
+    result = "Starlink";
+  } else {
+    result = "Starlink RV. Visit the FCC Broadband Map to check your options.";
+  }
+  
+  if (!strongConnection) {
+    result += " LTE router with external antenna or external antenna modification to hardware may be required";
+  }
+  
+  // Hide the question and button elements
+  question.style.display = "none";
+  buttons.style.display = "none";
+  
+  // Display the result
+  let resultElement = document.createElement("div");
+  resultElement.classList.add("result");
+  resultElement.textContent = result;
+  container.appendChild(resultElement);
+}
